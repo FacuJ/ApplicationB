589561b22769385606d8e4d03b334a00fa292055
Facundo Jaton <facundoe.jaton@gmail.com>
2021-02-21 11:57:26 -0300

    Getting data from A
    Getting data from Application A with ActivityResultLauncher

--- app/src/main/java/com/facundojaton/applicationb/MainActivity.kt
+++ app/src/main/java/com/facundojaton/applicationb/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.facundojaton.applicationb
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.facundojaton.applicationb.adapters.NotificationsListAdapter
 import com.facundojaton.applicationb.databinding.ActivityMainBinding
@@ -10,14 +14,45 @@ import com.facundojaton.applicationb.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var adapter = NotificationsListAdapter()
+    private lateinit var dataRetrievingRegister: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        dataRetrievingRegister = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.let { handleCustomData(it) }
+            } else {
+                dataNotRetrievedError()
+            }
+        }
+
         binding.rvNotifications.adapter = adapter
         if (intent?.action == "com.facundojaton.sharedata" && intent.type == "*/*") {
             handleCustomData(intent)
+        }
+        binding.btnRetrieveData.setOnClickListener {
+            retrieveDataFromA()
+        }
+    }
+
+    private fun dataNotRetrievedError() {
+        Toast.makeText(this, getString(R.string.data_not_retrieved), Toast.LENGTH_LONG).show()
+    }
+
+    private fun retrieveDataFromA() {
+        val getDataIntent: Intent = Intent().apply {
+            action = "com.facundojaton.getdata"
+            type = "*/*"
+        }
+        try {
+            dataRetrievingRegister.launch(getDataIntent)
+        } catch (exception: Exception) {
+            dataNotRetrievedError()
         }
     }
 
@@ -26,5 +61,10 @@ class MainActivity : AppCompatActivity() {
         if (data != null) {
             adapter.setNotifications(data)
         }
+    }
+
+    override fun onDestroy() {
+        dataRetrievingRegister.unregister()
+        super.onDestroy()
     }
 }
